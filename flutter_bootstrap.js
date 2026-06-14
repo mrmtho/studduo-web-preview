fc499c87734cccc75a507090e34224ccc2b494ad
mrmtho <mrmtho@users.noreply.github.com>
2026-06-14 11:53:31 +0200

    Deploy web build with half-sized library cards

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "897984541" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3359656657" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
