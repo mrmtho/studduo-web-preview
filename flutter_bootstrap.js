521c1119d3c3a31ab5b155c33ff651fdd816c2a1
mrmtho <mrmtho@users.noreply.github.com>
2026-06-15 18:31:29 +0200

    Deploy: Basic/Advanced segmented mode buttons

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "368506300" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3348293564" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
