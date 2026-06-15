f44d9e9f90120b0c7795372b4f9b5cfa8de7ab03
mrmtho <mrmtho@users.noreply.github.com>
2026-06-15 21:42:32 +0200

    Deploy: Fix overview navigation and synchronize GoRouter and local state

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3510609146" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "883452084" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
