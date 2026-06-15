79a634a8546b18a09bdc5ee689877444d3208377
mrmtho <mrmtho@users.noreply.github.com>
2026-06-15 11:00:09 +0200

    Update preview build for basic mode improvements

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4245574305" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1561645045" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
