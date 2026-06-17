ee21059e87d2cd4a3413d48bb017a6a71df93a4b
mrmtho <mrmtho@users.noreply.github.com>
2026-06-17 14:34:16 +0200

    Deploy web preview: Lyrics tab (distinct current/older surfaces, collapsible, immutable commits with cross-version diff colours)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "994213811" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3921472819" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
