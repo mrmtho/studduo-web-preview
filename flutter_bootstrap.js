2aae55af2f6125ae33f9fa908b79ef4cb6345643
mrmtho <mrmtho@users.noreply.github.com>
2026-06-17 16:49:50 +0200

    Deploy web preview: Soundstage themes (5 new presets, deep custom adjustor, full-app repaint)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3921472819" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1976238983" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
