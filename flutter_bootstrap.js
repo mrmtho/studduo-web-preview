e281996da8d378c7c0333976f27cb54fae8746ea
mrmtho <mrmtho@users.noreply.github.com>
2026-06-18 15:31:43 +0200

    Deploy web preview: live Firebase config (cloud sync enabled)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "105703691" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3532199664" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
