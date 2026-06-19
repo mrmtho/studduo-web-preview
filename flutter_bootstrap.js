04f485e921cccc38cede2363f0a661c96cc24a83
mrmtho <mrmtho@users.noreply.github.com>
2026-06-19 06:50:41 +0200

    Deploy web preview: DAW keyboard shortcuts (R/Del)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1886987939" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3445131209" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
